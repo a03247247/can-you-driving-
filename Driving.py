620fc91a58a7646dc2e82e57bc658a5a684ed084
Tsai <a03247247@gmail.com>
2021-04-13 01:59:03 +0800

    加入美國

--- Driving.py
+++ Driving.py
@@ -10,5 +10,12 @@ if country == '台灣' or country == 'Taiwan' :
     else:
         print('你還不能考駕照')
         print('還差' , 18-age,'歲')
+elif country == '美國' or country =='USA':
+    age = int(input('輸入年齡'))
+    if age >= 16:
+        print('你可以考駕照')
+    else:
+        print('你還不能考駕照')
+        print('還差' , 16-age,'歲')
 else:
     print('sorry 你不符合資格')
